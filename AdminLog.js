a120f7b6a0d7c219495441b8b0be41edd0aac38d
Ranyyahh <reyesran20@gmail.com>
2025-12-12 17:05:40 +0800

    Add files via upload

---
+++ AdminLog.js
@@ -0,0 +1,6 @@
+    const eye = document.getElementById('togglePassword');
+    const pass = document.getElementById('password');
+
+    eye.addEventListener('click', () => {
+        pass.type = pass.type === "password" ? "text" : "password";
+    });
