db7ac89906d4d034b5066485043859062e1074ae
CaptainSylv <gano.jervchristian.atienza@gmail.com>
2025-12-12 22:47:55 +0800

    Delete Admin Login directory

--- Admin Login/admin-login.js
+++
@@ -1,8 +0,0 @@
-
-    const eye = document.getElementById('togglePassword');
-    const pass = document.getElementById('password');
-
-    eye.addEventListener('click', () => {
-        pass.type = pass.type === "password" ? "text" : "password";
-    });
-
